ddad4a0e65f56034074aed7748c89ddb404d4a90
Kamal Bennani <kamalbouchboy1@hotmail.fr>
2019-10-13 15:20:04 +0200

    Add some modifiers and make the magic happen

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { observer } from "mobx-react";
 
-const App: React.FC = () => {
+import { Grid } from "./components/Grid";
+import { GlobalStyle } from "./style/globalStyle";
+import {
+  dijkstra,
+  astar,
+  greedyBestFirst,
+  depthFirstSearch,
+  breadthFirstSearch
+} from "./algorithms";
+import { AnimationType, INode } from "./models/node";
+import { uiStore, Algo } from "./store/UI";
+import { IGrid } from "./models/grid";
+import { asyncForEach, sleep } from "./utils";
+
+const App: React.FC = observer(() => {
+  const grid = uiStore.grid as IGrid;
+
+  const getAlgo = () => {
+    const algo = uiStore.algo as Algo;
+    switch (algo) {
+      case Algo.DIJKSTRA:
+        return dijkstra;
+      case Algo.ASTAR:
+        return astar;
+      case Algo.GREEDY:
+        return greedyBestFirst;
+      case Algo.DEPTH:
+        return depthFirstSearch;
+      case Algo.BREADTH:
+        return breadthFirstSearch;
+      default:
+        return dijkstra;
+    }
+  };
+
+  const handleClick = async () => {
+    const algoFunction = getAlgo();
+    const visitedNodes = algoFunction(grid, grid.startNode, grid.endNode);
+    const shortestPath = grid.getShortestPath(grid.endNode);
+
+    await asyncForEach(visitedNodes, async (node: INode) => {
+      await sleep(20);
+      node.setAnimate(AnimationType.VISITED_NODES);
+    });
+
+    await asyncForEach(shortestPath, async (node: INode) => {
+      await sleep(20);
+      node.setAnimate(AnimationType.PATH);
+    });
+  };
+
+  const handleChangeAlgo = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    uiStore.changeAlgo(+event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <button onClick={handleClick}>Start Algo</button>
+      <button onClick={uiStore.generateGrid}>Generate Grid</button>
+      <select onChange={handleChangeAlgo} value={uiStore.algo}>
+        <option value={Algo.DIJKSTRA}>Dijsktra</option>
+        <option value={Algo.ASTAR}>A*</option>
+        <option value={Algo.GREEDY}>Greedy Best First</option>
+        <option value={Algo.DEPTH}>Depth Best First</option>
+        <option value={Algo.BREADTH}>Breadth Best First</option>
+      </select>
+      <button onClick={uiStore.clearBoard}>Clean Board</button>
+      <Grid grid={grid} />
+      <GlobalStyle />
+    </React.Fragment>
   );
-}
+});
 
 export default App;
---
+++ src/style/globalStyle.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html, body, #root {
+    height: 100%;
+    overflow: hidden;
+  }
+`;
