e6cebfa0c668ba1a45bcf38a541f75994f13700f
Kamal Bennani <kamalbouchboy1@hotmail.fr>
2019-10-13 15:36:12 +0200

    Updates

---
+++ precache-manifest.7ca3f5e804fcb95778454b05f90df08a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc62b5f9956f75c8a97dc8e05d32d99e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e9d1c4393fcf7f6302b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "78572a95e080ca22d7c3",
+    "url": "/static/js/2.a24306eb.chunk.js"
+  },
+  {
+    "revision": "0e9d1c4393fcf7f6302b",
+    "url": "/static/js/main.b8d19d14.chunk.js"
+  },
+  {
+    "revision": "0061e02315ef4f63955e",
+    "url": "/static/js/runtime-main.7891003e.js"
+  }
+]);
